2507e13e66743e0cb0a4c7793ef01d3d353ed751
ssalter53 <ssalter@hurricanes.k12.hi.us>
2017-07-18 12:13:08 -1000

    js intial

---
+++ app.js
@@ -0,0 +1,52 @@
+var imgArray = ["cat4.jpg"];
+var cardArray = [];
+cardArray[0]=document.getElementById("c1");
+cardArray[1]=document.getElementById("c2");
+cardArray[2]=document.getElementById("c3");
+cardArray[3]=document.getElementById("c4");
+cardArray[4]=document.getElementById("c5");
+cardArray[5]=document.getElementById("c6");
+cardArray[6]=document.getElementById("c7");
+cardArray[7]=document.getElementById("c8");
+var i = 0;
+var imgElement = document.getElementById("c1");
+function on() {
+
+	var timeleft = 60;
+	var downloadTimer = setInterval(function(){
+	  document.getElementById("progressBar").value = 60 - --timeleft;
+	  if(timeleft > 0){
+	  }
+	  else{
+	  	clearInterval(downloadTimer);
+	  	alert('Times UP!');
+	  }  
+		
+	},1000);
+}
+var x = document.getElementById("c1");
+console.log(x);
+function flip(){
+	console.log("hi");
+	imgElement.src = cardArray[i];
+	if (x.style.backgroundImage==="dog3.jpg"&& cardArray[i]){
+		i = 1;
+	}else if(i === 0 && getElementById("c2")){
+		i = 2;
+	}else if(i === 0 && getElementById("c3")){
+		i = 3;
+	}else if(i === 0 && getElementById("c4")){
+		i = 4;
+	}else if(i === 0 && getElementById("c5")){
+		i = 5;
+	}else if(i === 0 && getElementById("c6")){
+		i = 6;
+	}else if(i === 0 && getElementById("c7")){
+		i = 7;
+	}else if(i === 0 && getElementById("c8")){
+		i = 8;
+	}else{
+		i = 0;
+	}
+}
+
